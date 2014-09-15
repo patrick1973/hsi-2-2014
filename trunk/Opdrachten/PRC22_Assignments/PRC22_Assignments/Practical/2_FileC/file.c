76a57a75ff0e4096b890dc55a8a6456da407b5f8
misteriepie@gmail.com <misteriepie@gmail.com@c9f2d63b-79d3-43b1-30ef-1670cda91228>
2014-09-15 21:10:32 +0000

    sorteer functie toegevoed en ook het verwijderen van een student

--- trunk/Opdrachten/PRC22_Assignments/PRC22_Assignments/Practical/2_FileC/file.c
+++ trunk/Opdrachten/PRC22_Assignments/PRC22_Assignments/Practical/2_FileC/file.c
@@ -15,15 +15,15 @@
 int ReadElement(FILE* FilePtr, int ElementNr, STUDENT* StudentPtr)
 {
 	int Result = 0;
-	
+
 	// TODO: implement
 	//controleer of de file pointers NULL is. zo ja dan retourneer -1;
 	if (FilePtr == NULL)
 	{
 		Result =  -1;
 	}
-	
-	//door loop het bestand. fseek berekend iedere keer de neuwe posite vanaf start. 
+
+	//door loop het bestand. fseek berekend iedere keer de neuwe posite vanaf start.
 	fseek(FilePtr,(ElementNr*sizeof(STUDENT)),SEEK_SET);
 	if (fread(StudentPtr,sizeof(STUDENT),1,FilePtr)!=0) // lees elke keer 1 record in. indien fread 0 retourneerd is er geen data meer.
 	{
@@ -39,7 +39,7 @@ int ReadElement(FILE* FilePtr, int ElementNr, STUDENT* StudentPtr)
 int WriteElement(FILE* FilePtr, int ElementNr, const STUDENT* StudentPtr)
 {
     int Result = 0;
-    
+
     // TODO: implement
 
     return Result;
@@ -56,7 +56,7 @@ ComputeAverageStudyResults (char* FileName, double* Average)
 	STUDENT student;
 	FILE *fp = NULL;
 	fp = fopen(FileName, "rb");
-	
+
 	if (fp!=NULL)
 	{
 		while(fread(&student,sizeof(STUDENT),1,fp) !=0)
@@ -78,7 +78,7 @@ ComputeAverageStudyResults (char* FileName, double* Average)
 	{
 		Result = -1;
 	}
-	
+
 	return Result;
 }
 
@@ -89,27 +89,34 @@ LinearSearchStudentsFile (char* FileName, int Number, STUDENT* StudentPtr)
 	int Result = 0;
 
 	// TODO: implement
-	// waar om kan ik student met nummer 1 niet vinden.
+	// waarom wordt het eerste element nooit gevonden
 	FILE *fp= NULL;
 	fp = fopen(FileName, "rb");
-	if (fp != NULL)
+	if (!fp)
 	{
-		while((fread(StudentPtr,sizeof(STUDENT),1,fp)) !=0)
+		Result = -1; // error tijdens het openen
+	}
+	else
+	{
+		//fread(StudentPtr,sizeof(STUDENT),1,fp);
+		do
 		{
-			if(StudentPtr->StudentNumber == Number)
+			if (StudentPtr->StudentNumber != Number)
 			{
-				Result = 0;	// nummer gevonden
+				printf("Niet gevonden\n");
+				Result = -1;
 			}
 			else
 			{
-				Result = -1;	// nummer niet gevonden
+				printf("Gevonden\n");
+				Result = 0;
+				fclose(fp);
+				return Result;
 			}
-		}
+			fread(StudentPtr,sizeof(STUDENT),1,fp);
+		} while(!feof(fp));
 	}
-	else 
-	{
-		Result = -1;
-	}
+	fclose(fp);
 	return Result;
 }
 
@@ -119,21 +126,26 @@ BinarySearchStudentsFile (char* FileName, int Number, STUDENT* StudentPtr)
     int Result = 0;
 
     // TODO: implement
-    
+
     return Result;
 }
 
-int 
+int
 AddStudentSortedToFile (char* FileName, STUDENT* StudentPtr)
 {
     int Result = 0;
 
     // TODO: implement
+    // voeg het nieuwe record toe
+    // nog maken dat de ingevoerde gegevens nog gecontroleerd wordt of het nummer al voor komt in de file
 	FILE *fp=NULL;
 	fp = fopen(FileName, "ab");
 	fwrite(StudentPtr,1,sizeof(STUDENT),fp);
 	fclose(fp);
-    
+
+
+    // Sorteer de file
+    sort();
     return Result;
 }
 
@@ -150,10 +162,10 @@ RemoveStudentFromFile (char* FileName, int StudentNumber)
 	STUDENT student = {0, "", 0} ;
 	FILE *fp= NULL;				// actuele file
 	FILE *fpTemp = NULL;		// tijdelijke file
-	
+
 	fp = fopen(FileName, "rb");
 	fpTemp = fopen("tijdelijke.dat" , "wb");
-	
+
 	if (fp != NULL)
 	{
 		fread(&student,sizeof(STUDENT),1,fp);
@@ -165,21 +177,18 @@ RemoveStudentFromFile (char* FileName, int StudentNumber)
 			}
 			fread(&student,sizeof(STUDENT),1,fp);
 		}
-		
+
 	}
-	
+
 	int sizeFp = ftell(fp);
 	int sizeFpTemp = ftell(fpTemp);
-	
-	printf("size fp %d\n",sizeFp);
-	printf("size fpTemp %d\n",sizeFpTemp);	
-	
+
 	fclose(fp);
 	fclose(fpTemp);
-	
+
 	remove("students.dat");
 	rename("tijdelijke.dat","students.dat");
-	
+
 	if ( sizeFp == sizeFpTemp) // als de grote van beide bestanden gelijk zijn is er dus niets gewijzigd, waarschijnlijk onbekend nummer
 	{
 		Result = -1;
@@ -190,4 +199,48 @@ RemoveStudentFromFile (char* FileName, int StudentNumber)
 	}
 	return Result;
 }
+void sort()
+    /* Bubbler sort
+    *   lees de eerste 2 records
+    *   draai ze om indien nodig zodat de kleinste links staat.
+    *   indien aan het einde dan staat het hoogste getal helemaal rechts.
+    *   zet de limit er een terug en herhaal het process
+    *   https://www.youtube.com/watch?v=JP5KkzdUEYI
+    */
+{
+    int StructureSize, Idx1, Idx2;                      // hulp variable
+    FILE * binaryFilePointer;                           // file pointer naar het bestand
+    binaryFilePointer = fopen("students.dat","rb+");    // open het bestand voor lezen en schrijven
+    STUDENT Index, IndexTemp;                           // maak tijdelijke structs aan
+    StructureSize = sizeof(Index);                      // leg vast hoe groot de struct is (bytes)
+    fseek(binaryFilePointer, 0, SEEK_END);              // zet de file pointer naar het einde van de file
+    int fileSize = ftell(binaryFilePointer);            // leg vast hoe groot de file is
+    rewind(binaryFilePointer);                          // en zet de file pointer terug naar het begin van de file
+
+    for (Idx1 = 0; Idx1 < fileSize; Idx1 += StructureSize) // limit bij houden.
+    {
+        for (Idx2 = 0; Idx2 < fileSize - StructureSize; Idx2 += StructureSize)
+        {
+            fread(&Index, StructureSize, 1, binaryFilePointer);
+            fread(&IndexTemp, StructureSize, 1, binaryFilePointer);
+
+            if (Index.StudentNumber > IndexTemp.StudentNumber)
+            {
+                fseek(binaryFilePointer, -(StructureSize * 2), SEEK_CUR); // pointer 2 elementen terug van huidig pos
+                fwrite(&IndexTemp, StructureSize, 1, binaryFilePointer);
+                fwrite(&Index, StructureSize, 1, binaryFilePointer);
+                fseek(binaryFilePointer, -StructureSize, SEEK_CUR);
+            }
+             else
+            {
+                fseek(binaryFilePointer, -StructureSize, SEEK_CUR);
+            }
+        }
+        rewind(binaryFilePointer);
+    }
+    fclose(binaryFilePointer);
+}
+
+
+
 
